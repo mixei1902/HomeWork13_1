9c3cf648f7520b333c723a5b20d04bfc7b9edd01
mixei1902 <misei190289@gmail.com>
2024-02-19 19:23:39 +0300

    inicial_2

--- test.py
+++ test.py
@@ -7,14 +7,17 @@ from main import Category, Product
 def category():
     return Category("Electronics", "Devices and gadgets", ["Laptop", "Phone"])
 
+
 @pytest.fixture
 def product():
     return Product("Laptop", "Portable computer", 1000.99, 10)
+
 
 def test_category_initialization(category):
     assert category.name == "Electronics"
     assert category.description == "Devices and gadgets"
     assert category.products == ["Laptop", "Phone"]
+
 
 def test_product_initialization(product):
     assert product.name == "Laptop"
@@ -22,8 +25,10 @@ def test_product_initialization(product):
     assert product.price == 1000.99
     assert product.quantity == 10
 
+
 def test_product_count(category):
     assert Category.total_unique_products == 2
+
 
 def test_category_count(category):
     assert Category.total_categories == 1
