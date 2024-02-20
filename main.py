087b3f92561d0ca86a588d1e5d514531264f9045
mixei1902 <misei190289@gmail.com>
2024-02-20 20:07:10 +0300

    inicial_4

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 class Category:
+    name: str
+    description: str
+    products: str
     total_categories = 0
     total_unique_products = 0
 
@@ -9,11 +12,15 @@ class Category:
         Category.total_categories += 1
         Category.total_unique_products += len(products)
 
+
 class Product:
+    name: str
+    description: str
+    price: float
+    quantity: list
+
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
         self.price = price
         self.quantity = quantity
-
-
