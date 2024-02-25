ad5d3c72a48e528484f973884b4de55f24779147
mixei1902 <misei190289@gmail.com>
2024-02-25 17:46:37 +0300

    HomeWork13_1

--- src/category.py
+++ src/category.py
@@ -1,13 +1,26 @@
 class Category:
     name: str
     description: str
-    products: str
+    products: list
     total_categories = 0
     total_unique_products = 0
 
-    def __init__(self, name, description, products):
+    def __init__(self, name: str, description: str, products: list):
         self.name = name
         self.description = description
-        self.products = products
+        self.products = products if products else []
         Category.total_categories += 1
         Category.total_unique_products += len(products)
+
+    def add_product(self, product):
+        self.products.append(product)
+        Category.total_unique_products += 1
+
+    @property
+    def products(self):
+        return "\n".join(
+            [f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт." for product in self.products])
+
+    @products.setter
+    def products(self, value):
+        self._products = value
--- src/product.py
+++ src/product.py
@@ -2,10 +2,25 @@ class Product:
     name: str
     description: str
     price: float
-    quantity: list
+    quantity: int
 
-    def __init__(self, name: str, description: str, price: float, quantity: list):
+    def __init__(self, name: str, description: str, price: float, quantity: int):
         self.name = name
         self.description = description
         self.price = price
         self.quantity = quantity
+
+    @classmethod
+    def create_product(cls, name, description, price, quantity):
+        return cls(name, description, price, quantity)
+
+    @property
+    def price(self):
+        return self.price
+
+    @price.setter
+    def price(self, value):
+        if value <= 0:
+            print("Цена введена некорректная")
+        else:
+            self.price = value
