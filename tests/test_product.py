835111262b37200f7eb1636cc749e67704bf8e50
mixei1902 <misei190289@gmail.com>
2024-02-27 10:07:01 +0300

    HomeWork14_1

--- tests/test_product.py
+++ tests/test_product.py
@@ -14,7 +14,7 @@ def test_product_initialization(product):
     assert product.price == 120
     assert product.quantity == 10
 
-def test_product():
+def test_product(product):
     product = Product("fruit", "fresh fruits", 120.0, 10)
     assert str(product) == "fruit, 120.0 руб. Остаток: 10 шт."
     assert product.price == 120.0
@@ -23,7 +23,9 @@ def test_product():
     product.price = -1
     assert product.price == 200.0
 
-def test_product_addition():
+def test_product_addition(product):
     product1 = Product("fruit", "fresh fruits", 100.0, 10)
     product2 = Product("fruit", "fresh fruits", 200.0, 20)
     assert product1 + product2 == 100.0 * 10 + 200.0 * 20
+
+
